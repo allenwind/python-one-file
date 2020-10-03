f6c8eaf66e09351fa90c49447a7827993502f24f
allenwind <allenwind@foxmail.com>
2020-10-03 22:48:56 +0800

    auto push

--- program/core.py
+++ program/core.py
@@ -1,7 +1,6 @@
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 
-
 def run_server(port=8080):
     print("listen at", port)
     with TCPServer(("", port), SimpleHTTPRequestHandler) as httpd:
